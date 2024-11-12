918ee22e08ca1752d5c4bc1f87963051a6efe79d
Itamar Kamar <itamarkamar@Itamars-MacBook-Pro.local>
2024-11-12 15:35:19 -0800

    removed unused variables

--- app/api/goals/route.js
+++ app/api/goals/route.js
@@ -1,14 +1,14 @@
 import connectDB from '../../../lib/models/db';
 import Goal from '../../../lib/models/Goal';
 
-export async function GET(req) {
+export async function GET() {
     await connectDB();
 
     try {
         const goals = await Goal.find();
         return new Response(JSON.stringify(goals), { status: 200 });
     } catch (error) {
-        return new Response(JSON.stringify({ error: 'Failed to fetch goals' }), { status: 500 });
+        return new Response(JSON.stringify({ error: 'Failed to fetch goals due to - ' + error }), { status: 500 });
     }
 }
 
@@ -21,6 +21,6 @@ export async function POST(req) {
         await newGoal.save();
         return new Response(JSON.stringify(newGoal), { status: 201 });
     } catch (error) {
-        return new Response(JSON.stringify({ error: 'Failed to save goal' }), { status: 400 });
+        return new Response(JSON.stringify({ error: 'Failed to save goal due to - ' + error}), { status: 400 });
     }
 }
